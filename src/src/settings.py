7f9918658150178fa4aef91b29258bf0491dce94
wle831 <wle831@gmail.com>
2015-02-16 12:50:05 -0800

    more files

--- src/src/settings.py
+++ src/src/settings.py
@@ -87,3 +87,11 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(BASE_DIR), "static", "templates"),
 )
+
+if DEBUG: 
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static-only")
+    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
+    STATICFILES_DIRS = (
+        os.path.join(os.path.dirname(BASE_DIR), "static", "static"),
+    )
--- src/src/urls.py
+++ src/src/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, include, url
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,3 +13,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL,
+								document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL,
+								document_root=settings.MEDIA_ROOT)
